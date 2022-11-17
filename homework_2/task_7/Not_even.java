b221168cb93b55917e5348bcbd240bf1d6b4cda2
Alexander Fomin <RezviyBelorus@gmail.com>
2022-11-17 08:00:02 +0100

    Update Not_even.java

--- homework_2/task_7/Not_even.java
+++ homework_2/task_7/Not_even.java
@@ -1,6 +1,7 @@
 package org.example.homework_2.task_7;
 
-public class Not_even {
+public class Not_even { // Not_even -> нижнее подчеркивания не используй в названии классов + второе словое с большой буквы. 
+    // А если совсем правильно, то Odd название класса сюда хороошо подходит
     public static void main(String[] args) {
         int j = 0;
         for (int i = 0; i <= 100; i++) {
